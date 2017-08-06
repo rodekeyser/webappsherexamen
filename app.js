926a6c68fe16b39eb978406e56d1705908f94a32
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2017-08-06 22:38:18 +0200

    added some things

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
-var app = angular.module('gamecorner', []);
+var app = angular.module('gamecorner', ['ui.router']);
+
+app.factory('games', [function () {
+    var o = {
+        games: []
+    };
+    return o;
+}])
 
 app.controller('MainCtrl', [
     '$scope',
-    function($scope){
+    'games',
+    function ($scope, games) {
         $scope.test = 'Hello gamers!';
-        $scope.games = [
-            {title:'assassins creed', upvotes:25, favorite: true},
-            {title:'mass effect', upvotes:36, favorite: true},
-            {title:'fifa', upvotes:60, favorite: false},
-            {title:'rocket league', upvotes:12, favorite: false}
-        ];
-        $scope.addGame = function(){
-            $scope.games.push({title:$scope.title, upvotes: 0, favorite: false});
-            $scope.title='';
+        $scope.games = games.games;
+        $scope.addGame = function () {
+            if (!$scope.title || $scope.title === '') { return; }
+            $scope.games.push({ title: $scope.title, link: $scope.link, upvotes: 0, favorite: false });
+            $scope.title = '';
+            $scope.link = '';
         };
+        $scope.incrementUpvotes = function (game) {
+            game.upvotes += 1;
+        };
+    }
+]);
+
+app.controller('GameCtrl',[
+    '$scope',
+    '$stateParams',
+    'games',
+    function($scope, $stateParams, games){
+
+    }
+]);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl'
+        });
+
+        $stateProvider.state('games', {
+            url: '/games/{id}',
+            templateUrl: '/games.html',
+            controller: 'GameCtrl'
+        });
+
+        $urlRouterProvider.otherwise('home');
     }
 ]);
